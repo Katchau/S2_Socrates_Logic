8843ce3a5c0fa5477b7ab74e84b4a652caaedb1b
Katchau <joaohccl@live.com.pt>
2016-11-06 12:41:27 +0000

    Fixed usefull things 💃

--- proj1/usefull prepositions.pl
+++ proj1/usefull prepositions.pl
@@ -1,21 +1,6 @@
-append([],L,L).
-append([X|L1],L2,[X|L3]):- append(L1,L2,L3).
+load_libraries:- use_module(library(lists)).
 
-member(X, [X | Xs]).
-member(X, [Y|Ys]):- member(X, Ys).
-prefix([], Ys).
-prefix([X | Xs], [X | Ys]):- prefix(Xs, Ys).
-suffix(Xs, Xs).
-suffix(Xs, [Y | Ys]):- suffix(Xs, Ys).
-sublist(Xs,Ys):- prefix(Ps, Ys), suffix(Xs, Ps).
-sublist(Xs, Ys):- prefix(Xs, Ss), suffix(Ss, Ys).
-reverse(Xs, Ys):- reverse(Xs, [], Ys).
-reverse([X|Xs], Acc, Ys):- reverse(Xs, [X|Acc], Ys).
-reverse([], Ys, Ys).
-length([], 0).
-length([X|Xs], N):- N1 is N-1, length(Xs, N1).
-delete([X|Xs], X, Ys):- delete(Xs, X, Ys).
-delete([X|Xs], Z, [X|Ys]):- X=\=Z, delete(Xs, Z, Ys).
-delete([], X, []).
-select(X, [X|Xs], Xs).
-select(X,[Y|Ys], [Y | Zs]):- select(X, Ys, Zs). 
+readCoords(X,Y):- write('X= '), read(XX),
+				  ((XX > 10 ; XX < 1) -> X = 1; X = XX),
+				  nl, write('Y= '), read(YY),
+				  ((YY > 10 ; YY < 1) -> Y = 1; Y = YY).
