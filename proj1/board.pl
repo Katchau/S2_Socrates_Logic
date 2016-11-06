8cd7b84018d2628648132126086455cd486051fb
Jonas L <joaohccl@live.com.pt>
2016-11-06 17:40:02 +0000

    select piece done

--- proj1/board.pl
+++ proj1/board.pl
@@ -71,13 +71,17 @@ traduz(x) :- put_code(11093). %write(' X').
 
 
 
-perc_choice([L1 | Ls], N, L) :- N == L -> display_line(L1);  perc_board(Ls, N, L).
-perc_board([L1 | Ls], N, L) :- L2 is L + 1 ,  perc_choice([L1 | Ls], N, L2).
+select_piece_aux(X, X1, [L | Ls], Peca):- (X == X1, Peca = L , !) ; (X2 is X1 + 1, select_piece_aux(X, X2, Ls, Peca)).
+select_piece_aux(X, Y, Y1, [L | Ls], Peca):- (Y == Y1 , !,  select_piece_aux(X, 1,L , Peca) ); (Y2 is Y1 + 1 , select_piece_aux(X, Y, Y2, Ls, Peca)). 
+select_piece([L | Ls],X , Y, Peca):- X =< 10, X >= 1, Y =< 10, X >= 1, select_piece_aux(X, Y, 1, [L | Ls], Peca).
 
-display_specific_line([L1 | Ls], N) :- perc_board([L1 | Ls], N, 0 ).
+%perc_choice([L1 | Ls], N, L) :- N == L -> display_line(L1);  perc_board(Ls, N, L).
+%perc_board([L1 | Ls], N, L) :- L2 is L + 1 ,  perc_choice([L1 | Ls], N, L2).
 
-destroy_end([L1 | Ls], X, X1):- X == X1 -> traduz(L1) ; destroy_ex(Ls , X, X1).
-destroy_ex([L1 | Ls], X, X1):- X2 is X1 + 1, destroy_end([L1 | Ls], X, X2).
-destroy_line([L1 | Ls], X, Y, X1, Y1) :- Y == Y1 -> destroy_ex(L1, X, X1);  destroy_rec(Ls , X, Y, X1, Y1).
-destroy_rec([L1 | Ls], X, Y, X1, Y1):- Y2 is Y1 + 1, destroy_line([L1 | Ls], X, Y, X1, Y2).
-destroy_peca([L1 | Ls], X, Y):- destroy_rec([L1 | Ls], X, Y, 0, 0).
+%display_specific_line([L1 | Ls], N) :- perc_board([L1 | Ls], N, 0 ).
+
+%destroy_end([L1 | Ls], X, X1):- X == X1 -> traduz(L1) ; destroy_ex(Ls , X, X1).
+%destroy_ex([L1 | Ls], X, X1):- X2 is X1 + 1, destroy_end([L1 | Ls], X, X2).
+%destroy_line([L1 | Ls], X, Y, X1, Y1) :- Y == Y1 -> destroy_ex(L1, X, X1);  destroy_rec(Ls , X, Y, X1, Y1).
+%destroy_rec([L1 | Ls], X, Y, X1, Y1):- Y2 is Y1 + 1, destroy_line([L1 | Ls], X, Y, X1, Y2).
+%destroy_peca([L1 | Ls], X, Y):- destroy_rec([L1 | Ls], X, Y, 0, 0).
