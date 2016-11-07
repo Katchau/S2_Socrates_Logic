a3c3cc17de9079e292e7bbe722d6aaf96752ff22
Katchau <joaohccl@live.com.pt>
2016-11-07 23:33:58 +0000

    Jumpy :video_camera:

--- proj1/movement restrictions.pl
+++ proj1/movement restrictions.pl
@@ -1,6 +1,6 @@
 :- include('usefull prepositions.pl').
 
-
+check_ortho_aux(Board, X, Y, Peca):- X == 0; Y == 0; Y == 11; X == 1 .
 check_ortho_aux(Board, X, Y, Peca):- select_piece(Board, X, Y, Other), Other \== Peca.
 check_ortho_adjacency(Board, X, Y):- select_piece(Board, X, Y, Peca), Peca \== v, !, X1 is X + 1, X2 is X-1, Y1 is Y +1, Y2 is Y-1,
 									 check_ortho_aux(Board, X1, Y, Peca), !, check_ortho_aux(Board, X, Y1, Peca), !,
@@ -37,7 +37,8 @@ replace_element_board([L | Ls], [L1 | Ls1], X, Y, Y1, Peca):- Y == Y1, Y2 is Y1 
 replace_element_board([L | Ls], [L | Ls1], X, Y, Y1, Peca):- Y \== Y1, Y2 is Y1 + 1, replace_element_board(Ls, Ls1, X, Y, Y2, Peca) ,!.
 
 
-jump(Board, X, Y):- X == 0; Y == 0; check_ortho_adjacency(Board, X, Y).
-jump(Board, X0, Y0, X, Y):- X \== X0, Y0 == Y, X_F is X + (X-X0), jump(Board, X_F, Y).
-jump(Board, X0, Y0, X, Y):- Y \== Y0, X0 == X, Y_F is Y + (Y-Y0), jump(Board, X, Y_F).
-jump(Board, Player, X0, Y0, X, Y):- select_piece(Board, X, Y, Peca), Peca \== Player, jump(Board, X0, Y0, X, Y).
+jump_aux(Board, Peca, X, Y):- X == 0; Y == 0; X == 11; Y == 11 .
+jump_aux(Board, Peca, X, Y):- select_piece(Board, X, Y, Vazio), Vazio == v, !, replace_element_board(Board, Test, X, Y, 1, Peca),  !, check_ortho_adjacency(Test, X, Y).
+jump_aux(Board, Peca, X0, Y0, X, Y):- X \== X0, Y0 == Y, X_F is X + (X-X0), !, jump_aux(Board, Peca, X_F, Y), !.
+jump_aux(Board, Peca, X0, Y0, X, Y):- Y \== Y0, X0 == X, Y_F is Y + (Y-Y0), !, jump_aux(Board, Peca, X, Y_F), !.
+jump(Board, Player, X0, Y0, X, Y):- select_piece(Board, X, Y, Peca), Peca \== Player, Peca \== v, !, jump_aux(Board, Player, X0, Y0, X, Y).
