dddd071115610e335c696955a1792c7d7f91fcf5
Katchau <joaohccl@live.com.pt>
2016-11-08 21:53:01 +0000

    Add todos

--- proj1/movement restrictions.pl
+++ proj1/movement restrictions.pl
@@ -10,12 +10,12 @@ check_ortho_adjacency(Board, X, Y):- select_piece(Board, X, Y, Peca), Peca \== v
 
 
 check_ortho_adv_aux(Board, X, Y, Peca):- select_piece(Board, X, Y, Other), Other \== Peca, Other \== v.
-
+%amnh ver isto
 check_ortho_adv_adjacency(Peca, Board, X, Y):- select_piece(Board, X, Y, Peca), Peca \== v, !, X1 is X + 1, X2 is X-1, Y1 is Y +1, Y2 is Y-1,
-									                      (
+									    (
                                          check_ortho_adv_aux(Board, X1, Y, Peca), !;
                                          check_ortho_adv_aux(Board, X, Y1, Peca), !;
-									                       check_ortho_adv_aux(Board, X2, Y, Peca), !;
+									     check_ortho_adv_aux(Board, X2, Y, Peca), !;
                                          check_ortho_adv_aux(Board, X, Y2, Peca), !
                                         ).
 
@@ -38,14 +38,7 @@ check_center_move(Board_size, X0, Y0, X ,Y):- X0 =< Board_size / 2, Y0 > Board_s
 check_center_move(Board_size, X0, Y0, X ,Y):- X0 > Board_size / 2, Y0 > Board_size / 2, !, check_center_move(X0, Y0, X, Y, -1, -1) .
 check_center_move(Board_size, X0, Y0, X ,Y):- X0 =< Board_size / 2, Y0 =< Board_size / 2, !, check_center_move(X0, Y0, X, Y, 1, 1) .
 
-replace_element_list([],[], X, X1, Peca).
-replace_element_list([H | L1], [Peca | L2], X, X1, Peca):- X == X1, X2 is X1 + 1, !, replace_element_list(L1, L2, X, X2, Peca).
-replace_element_list([H | L1], [H | L2], X, X1, Peca):- X \== X1 , X2 is X1 + 1, !, replace_element_list(L1, L2, X, X2, Peca).
-
-replace_element_board([],[], X, Y, Y1, Peca).
-replace_element_board([L | Ls], [L1 | Ls1], X, Y, Y1, Peca):- Y == Y1, Y2 is Y1 + 2, replace_element_list(L,L1,X,1,Peca), !, replace_element_board(Ls, Ls1, X, Y, Y2, Peca).
-replace_element_board([L | Ls], [L | Ls1], X, Y, Y1, Peca):- Y \== Y1, Y2 is Y1 + 1, replace_element_board(Ls, Ls1, X, Y, Y2, Peca) ,!.
-
+%check_mov_adjoining(Board, X, Y):- check_ortho_adv_adjacency_dest(Peca, Board, X, Y). 
 
 jump_aux(Board, Peca, X, Y):- check_outOfBounds(X , Y).
 jump_aux(Board, Peca, X, Y):- select_piece(Board, X, Y, Vazio), Vazio == v, replace_element_board(Board, Test, X, Y, 1, Peca),  !, check_ortho_adjacency(Test, X, Y).
--- proj1/usefull prepositions.pl
+++ proj1/usefull prepositions.pl
@@ -6,3 +6,11 @@ readCoords(X,Y):- write('X= '), read(XX),
 				  ((XX > 10 ; XX < 1) -> X = 1; X = XX),
 				  nl, write('Y= '), read(YY),
 				  ((YY > 10 ; YY < 1) -> Y = 1; Y = YY).
+				  
+replace_element_list([],[], X, X1, Peca).
+replace_element_list([H | L1], [Peca | L2], X, X1, Peca):- X == X1, X2 is X1 + 1, !, replace_element_list(L1, L2, X, X2, Peca).
+replace_element_list([H | L1], [H | L2], X, X1, Peca):- X \== X1 , X2 is X1 + 1, !, replace_element_list(L1, L2, X, X2, Peca).
+
+replace_element_board([],[], X, Y, Y1, Peca).
+replace_element_board([L | Ls], [L1 | Ls1], X, Y, Y1, Peca):- Y == Y1, Y2 is Y1 + 2, replace_element_list(L,L1,X,1,Peca), !, replace_element_board(Ls, Ls1, X, Y, Y2, Peca).
+replace_element_board([L | Ls], [L | Ls1], X, Y, Y1, Peca):- Y \== Y1, Y2 is Y1 + 1, replace_element_board(Ls, Ls1, X, Y, Y2, Peca) ,!.
