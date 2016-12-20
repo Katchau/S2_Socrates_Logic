5e41f7483772c5975376501986aaec542a825e53
Bruno Barros <brunocostabarros@hotmail.com>
2016-12-20 16:08:04 +0000

    Tests and TPC initialized

--- proj2/aulinhas.pl
+++ proj2/aulinhas.pl
@@ -1,9 +1,56 @@
-%Todos os patinhos sabem bem nadar 
-%Sabem bem nadar 
-%Cabeça para baixo, rabinho para o ar 
-%Cabeça para baixo, rabinho para o ar 
-%
-%Quando estão cansados da água vão sair 
-%Da água vão sair 
-%Depois em grande fila, para o ninho querem ir 
-%Depois em grande fila, para o ninho querem ir
+:- use_module(library(clpfd)).
+:- use_module(library(lists)).
+
+criaCalendario(NumSemanas, Calendario):-
+    NumDias is (NumSemanas * 5),
+    length(Calendario, NumDias).
+
+verificaTestesAno(NumSemanas, Disciplinas):-
+    criaCalendario(NumSemanas, Calendario),
+    domain(Disciplinas,0,2),
+    domain(Calendario,0,1),
+    length(Disciplinas, Tamanho),
+    NumTestes is (Tamanho * 2),
+    sum(Disciplinas, #=, NumTestes),
+    sum(Calendario, #=, NumTestes),
+    verificaTestesTodasSemanas(Calendario),
+    labeling([], Calendario),
+    imprimeCalendario(Calendario),
+    labeling([], Disciplinas).
+
+verificaTestesTodasSemanas([]).
+verificaTestesTodasSemanas([Dia1, Dia2, Dia3, Dia4, Dia5  | Resto]):-
+    verificaTestesMesmaSemana([Dia1, Dia2, Dia3, Dia4, Dia5]),
+    verificaTestesTodasSemanas(Resto).
+
+verificaTestesMesmaSemana(Dias):-
+    sum(Dias, #=< ,2),
+    verfificaTestesDiasConsecutivos(Dias).
+
+verfificaTestesDiasConsecutivos([]).
+verfificaTestesDiasConsecutivos([Dia]):-
+    Dia #=< 1.
+verfificaTestesDiasConsecutivos([Dia1 | [Dia2 | Resto]]):-
+    Dia1 + Dia2 #=< 1,
+    verfificaTestesDiasConsecutivos([Dia2 | Resto]).
+
+verificaTPCNoDia([],_,_).
+verificaTPCNoDia([Dia | Resto], N, DiaLivre):-
+    N =:= DiaLivre,
+    Dia #= 0,
+    N1 is (N + 1),
+    verificaTPCNoDia(Resto, N1, DiaLivre).
+verificaTPCNoDia([Dia | Resto], N, DiaLivre):-
+    Dia #=< 2,
+    N1 is (N + 1),
+    verificaTPCNoDia(Resto, N1, DiaLivre).
+
+verificaTPCMesmaSemana(Dias):-
+    domain(Dias,0,2),
+    verificaTPCNoDia(Dias, 1, 3),
+    labeling([], Dias).
+
+imprimeCalendario([]).
+imprimeCalendario([Segunda, Terca, Quarta, Quinta, Sexta | Resto]):-
+    format("Segunda:~w Terca:~w Quarta:~w Quinta:~w Sexta:~w ~n",[Segunda, Terca, Quarta, Quinta, Sexta]),
+    imprimeCalendario(Resto).
